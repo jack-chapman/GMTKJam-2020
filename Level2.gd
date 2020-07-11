65cd4da49fc5c93d7af5607a6ac5017b522ad778
jack-chapman <jackchapman093@gmail.com>
2020-07-11 14:14:37 +0100

    Add kill floor thing and reset

--- Level2.gd
+++ Level2.gd
@@ -4,6 +4,8 @@ onready var left_arrow = $CanvasLayer/LeftArrow
 onready var right_arrow = $CanvasLayer/RightArrow
 onready var jump_arrow = $CanvasLayer/JumpArrow
 
+onready var player_spawn = $PlayerSpawn
+
 
 
 func _on_Player_lose_right_control():
@@ -30,3 +32,16 @@ func _on_Player_fix():
 func _on_Finish_body_entered(_body):
 	# load level 3
 	print('finished l2')
+
+
+func _on_Kill_body_entered(body):
+	body.fix()
+	body.velocity = Vector2.ZERO
+	body.global_position = player_spawn.global_position
+
+func _input(event):
+	if (event.is_action_pressed('reset')):
+		var player = $Player
+		player.fix()
+		player.velocity = Vector2.ZERO
+		player.global_position = player_spawn.global_position
